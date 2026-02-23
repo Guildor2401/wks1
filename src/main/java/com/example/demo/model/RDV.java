aaaf78e3549cbd6ae95e5d833b5152a0a72efe26
Guildor2401 <guildor240199@gmail.com>
2026-02-23 19:48:19 +0100

    test

---
+++ src/main/java/com/example/demo/model/RDV.java
@@ -0,0 +1,30 @@
+package com.example.demo.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Entity
+@Data
+@NoArgsConstructor
+@Builder
+@AllArgsConstructor
+public class RDV {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private Date date;
+    private Integer hours;
+    @OneToOne
+    @JoinColumn(name = "client_id")
+    private  Client client;
+    private Integer status;
+    @ManyToOne
+    @JoinColumn(name = "professionels_id")
+    private Professionnel professionnel;
+}
