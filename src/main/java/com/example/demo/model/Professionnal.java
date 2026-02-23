5725ab2aca4b627e07b2d8aed7c793cbd1b6b0c3
herve1986 <vandendrisse.herve@gmail.com>
2026-02-23 20:37:30 +0100

    Repository

--- src/main/java/com/example/demo/model/Professionnal.java
+++ src/main/java/com/example/demo/model/Professionnal.java
@@ -4,20 +4,22 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data
 @NoArgsConstructor
 @Builder
 @AllArgsConstructor
-public class Client {
+public class Professionnal {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    private String firstName;
-    private String lastName;
-    private String email;
-    private Integer telephone;
+    private String name;
+    private String metier;
 }
+
