f28fb5e8c1c6710d37a5bc944ee822b409274530
Florian <fdonckersf@gmail.com>
2026-02-23 20:15:10 +0100

    DB

---
+++ README.md
@@ -0,0 +1,2 @@
+DB connexion string
+postgresql://neondb_owner:npg_sceZ8Uzm7whH@ep-little-shape-ai4417qg-pooler.c-4.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require
--- src/main/java/com/example/demo/RDVApplication.java
+++ src/main/java/com/example/demo/RDVApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RDVApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(RDVApplication.class, args);
 	}
-
 }
