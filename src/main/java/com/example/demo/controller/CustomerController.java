ba7b32539105d0757513478bcbd6e99fad49b43d
herve1986 <vandendrisse.herve@gmail.com>
2026-02-24 19:05:18 +0100

    controller

--- src/main/java/com/example/demo/controller/CustomerController.java
+++ src/main/java/com/example/demo/controller/CustomerController.java
@@ -1,12 +1,30 @@
 package com.example.demo.controller;
 
+import com.example.demo.model.Customer;
+import com.example.demo.reposirtory.CustomerRepositoryy;
+import com.example.demo.service.CustomerService;
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @AllArgsConstructor
-@RequestMapping("")
+@RequestMapping("api/customer")
 public class CustomerController {
+    CustomerService customerService;
 
+    @GetMapping("/AllCustomers")
+    public List<Customer> GetAllCustomers()
+    {
+        return customerService.GetAllCustomers();
+    }
+
+    @GetMapping("/GetOneCustomer")
+    public Customer GetOneCustomer(Integer id)
+    {
+        return customerService.getCustomerById(id);
+    }
 }
